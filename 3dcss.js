1e001b894bb06b07ff7cc0fb4ad2726c16bb5311
kimkyungsub <cyon256@icloud.com>
2023-08-19 10:08:49 +0900

    scene

--- 3dcss.js
+++ 3dcss.js
@@ -1,6 +1,14 @@
 const body = document.querySelector("body");
-const card = body.querySelector(".card");
-
-card.addEventListener("click", function (e) {
-  card.classList.toggle("is-flipped");
+const form = body.querySelector("form");
+const cube = document.querySelector(".cube");
+let currentClass = "";
+form.addEventListener("change", () => {
+  const checkedRadio = form.querySelector(":checked");
+  const showClass = "show-" + checkedRadio.value;
+  if (currentClass) {
+    cube.classList.remove(currentClass);
+  }
+  console.log(currentClass);
+  cube.classList.add(showClass);
+  currentClass = showClass;
 });
